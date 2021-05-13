b69136ae85c512b3e2c9dab8b27ea5ef1ffbaf5a
arcodev <chris_arco0794@hotmail.com>
2021-05-13 15:47:52 -0500

    Terminacion de la animacion del body al hacer scroll

--- build/js/app.js
+++ build/js/app.js
@@ -43,23 +43,51 @@ function changeCircle(color) {
     const circle = document.querySelector('.circle');
     circle.style.background = color;
 }
+const service = document.getElementById('service');
+
+function txtServices() {
+    service.innerHTML = 'Publicidad'
+}
+
+function txtServices2() {
+    service.innerHTML = 'Marketing';
+}
+
+function txtServices3() {
+    service.innerHTML = 'Social Media';
+}
+
+function txtServices4() {
+    service.innerHTML = 'Tomas con dron';
+}
+
+function txtServices5() {
+    service.innerHTML = 'Fotografia';
+}
+
+function txtServices6() {
+    service.innerHTML = 'Video';
+}
+
+function txtServices7() {
+    service.innerHTML = 'Redes Sociales';
+}
 
 window.onscroll = function () {
-    var y = window.scrollY;
+    const scroll = window.scrollY;
     const body = document.querySelector('body');
-    const scr = document.getElementById('scroll');
-    console.log(y);
-    //scr.innerHTML = y;
-    if (y > 980) {
+    const circle = document.getElementById('circle');
+    if (scroll > 980) {
         body.style.background = '#F6BB31';
-    } 
-    if (y > 1700) {
+    }
+    if (scroll > 1700) {
         body.style.background = '#6EB2BD';
     }
-    if(y > 2400) {
-        body.style.background = '#B22777';
-    }
-    if(y < 980) {
+    if (scroll > 2400) {
         body.style.background = '#594590';
+    }
+    if (scroll < 980) {
+        body.style.background = '#B22777';
+
     }
 }
