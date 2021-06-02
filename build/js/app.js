c22a9a9db647eee0239362dd0a7eb139e23c27a7
arcodev <chris_arco0794@hotmail.com>
2021-06-02 18:30:38 -0500

    Preparando la seccion del filtro del portafolio

--- build/js/app.js
+++ build/js/app.js
@@ -1,13 +1,24 @@
 //Animacion en el video  al hacer scrooll 
 window.addEventListener('DOMContentLoaded', function () {
     const bars = document.getElementById('bars');
+    const close = document.getElementById('close');
     const overlay = document.getElementById('overlay');
     const link = document.getElementById('link');
     if (bars) {
         bars.addEventListener('click', function () {
-            overlay.style.transform = 'unset';
+            //overlay.style.transform = 'unset';
+            overlay.classList.remove('quitarOverlay');  
             overlay.classList.add('overlay');  
-            bars.style.display = 'none';      
+            bars.style.display = 'none';
+            close.style.display = 'block';      
+        });
+    }
+    if(close) {
+        close.addEventListener('click', function() {
+            overlay.style.transform = 'transalte(-100%)';
+            overlay.classList.add('quitarOverlay');  
+            close.style.display = 'none'; 
+            bars.style.display = 'block';
         });
     }
     /*if(link) {
