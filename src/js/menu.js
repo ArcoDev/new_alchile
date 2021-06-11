4dd1630aaecf04364e16f552cac913839512fcdc
arcodev <chris_arco0794@hotmail.com>
2021-06-11 18:24:59 -0500

    Ajuste en la transicion del menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,11 +3,15 @@ window.addEventListener('DOMContentLoaded', function () {
     const bars = document.getElementById('bars');
     const close = document.getElementById('close');
     const overlay = document.getElementById('overlay');
+    const layer = document.querySelector('.layer-menu');
     if (bars) {
         bars.addEventListener('click', function () {
             //overlay.style.transform = 'unset';
             overlay.classList.remove('quitarOverlay');
             overlay.classList.add('overlay');
+            setTimeout(function () {
+                layer.classList.add('quitarTransform');
+            }, 2800)
             bars.style.display = 'none';
             close.style.display = 'block';
         });
